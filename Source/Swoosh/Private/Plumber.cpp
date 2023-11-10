79289ac33fd360c711ae03bd053ff0f53a4e7d82
Samay28 <imsamaystar@gmail.com>
2023-11-10 12:46:25 +0530

    basic controller

---
+++ Source/Swoosh/Private/Plumber.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Plumber.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SpotLightComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+
+// Sets default values
+APlumber::APlumber()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpingArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 0;
+
+	ViewCam = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	ViewCam->SetupAttachment(SpringArm);
+
+	FlashLight = CreateDefaultSubobject<USpotLightComponent>(TEXT("FlashLight"));
+	FlashLight->SetupAttachment(ViewCam);
+}
+
+// Called when the game starts or when spawned
+void APlumber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController *PlayerController = Cast<APlayerController>(GetController()))
+	{
+		UEnhancedInputLocalPlayerSubsystem *Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+
+		if (Subsystem)
+		{
+			Subsystem->AddMappingContext(PlumberMappingContext, 0);
+		}
+	}
+}
+
+// Called every frame
+void APlumber::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APlumber::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent *EIC = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EIC->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlumber::LookCharacter);
+	}
+}
+
+void APlumber::LookCharacter(const FInputActionValue &Value)
+{
+	FVector2D LookAxisValue = Value.Get<FVector2D>();
+
+	float YawInput = LookAxisValue.X * Xsensi;
+	float PitchInput = LookAxisValue.Y * Ysensi;
+
+	if (GetController())
+	{
+		AddControllerYawInput(YawInput);
+		AddControllerPitchInput(PitchInput);
+	}
+}
---
+++ Source/Swoosh/Public/Plumber.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "Plumber.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class FInput;
+class UCameraComponent;
+class USpringArmComponent;
+class USpotLightComponent;
+
+UCLASS()
+class SWOOSH_API APlumber : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlumber();
+
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext *PlumberMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction *LookAction;
+
+	UPROPERTY(EditAnywhere, Category = Variables)
+	float Ysensi;
+
+	UPROPERTY(EditAnywhere, Category = Variables)
+	float Xsensi;
+
+
+
+	void LookCharacter(const FInputActionValue &Value);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent *ViewCam;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent *SpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	USpotLightComponent *FlashLight;
+
+	UPROPERTY(EditAnywhere, Category = "Head Bob")
+	float BobSpeed = 10.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Head Bob")
+	float BobAmount = 10.0f;
+};
