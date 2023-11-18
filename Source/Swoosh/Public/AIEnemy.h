5c79b2b656e6c440fcd71aafffdb9259b23d8288
Samay28 <imsamaystar@gmail.com>
2023-11-18 19:11:46 +0530

    AI Started

---
+++ Source/Swoosh/Public/AIEnemy.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AIEnemy.generated.h"
+
+UCLASS()
+class SWOOSH_API AAIEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAIEnemy();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+
+};
