ebd4f7c8046474da38cbee826a5ecb7c2bfbcaac
Samay28 <imsamaystar@gmail.com>
2023-11-19 13:16:36 +0530

    enemy moves

---
+++ Source/Swoosh/Private/BTTask_FindRandomLocation.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_FindRandomLocation.h"
+#include "NavigationSystem.h"
+#include "Enemy_AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_FindRandomLocation::UBTTask_FindRandomLocation(FObjectInitializer const& ObjectInitializer)
+{
+    NodeName = "Find Random Location In NavMesh";
+}
+EBTNodeResult::Type UBTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{   
+    //get AI controller and enemy
+    if(auto* const cont = Cast<AEnemy_AIController>(OwnerComp.GetAIOwner()))
+    {
+        if(auto* const enemy = cont->GetPawn())
+        {   
+            //obtain enemy location to use as an origin
+            auto const Origin = enemy->GetActorLocation();
+
+            //get nav and generate random location
+            if(auto* const NavSys = UNavigationSystemV1::GetCurrent(GetWorld()))
+            {   
+                FNavLocation loc;
+                if(NavSys->GetRandomPointInNavigableRadius(Origin, SearchRadius, loc))
+                {
+                    OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), loc.Location);
+                }
+
+                //finish with success
+                FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+                return EBTNodeResult::Succeeded;
+            }
+        }
+    }
+    return EBTNodeResult::Failed;
+}
